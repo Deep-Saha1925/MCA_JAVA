adcc6bc1d0c987704fd589be08f642bda2675f3f
Deep Saha <dip23447@gmail.com>
2026-04-06 15:49:09 +0530

    Added sum

--- A1/P1.java
+++ A1/P1.java
@@ -1,13 +1,16 @@
 public class P1{
 
     void fibo(int n){
-        int a = 0, b = 1, c = 0;
+        int a = 0, b = 1, c = 0, sum = 0;
         for(int i=1; i<=n; i++){
+            sum += a;
             System.out.print(a + ", ");
             c = a + b;
             a = b;
             b = c;
         }
+
+        System.out.print("\nSum is: " + sum);
     }
 
     public static void main(String args[]){
