9880b6d7c8f5aee86552733e689e4ecabba8090c
Deep Saha <dip23447@gmail.com>
2026-04-08 11:23:36 +0530

    P16

---
+++ A1/P16.java
@@ -0,0 +1,11 @@
+public class P16 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 105; i < 200; i += 7) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
