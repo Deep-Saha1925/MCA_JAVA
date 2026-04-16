187557e694f172aee377668432667a07d1bffe1e
Deep Saha <dip23447@gmail.com>
2026-04-16 23:07:56 +0530

    Modified

--- A1/P2.java
+++ A1/P2.java
@@ -1,9 +1,8 @@
 public class P2{
 
     boolean isPrime(int n){
-        for(int i=2; i<n; i++){
+        for(int i=2; i<n; i++)
             if(n % i == 0) return false;
-        }
 
         return true;
     }
