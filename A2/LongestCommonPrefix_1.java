14d7faa820b785cb86903f2631c972e5f54df7cd
Deep-Saha1925 <dip23447@gmail.com>
2026-05-04 14:09:10 +0530

    Longest Common prefix

---
+++ A2/LongestCommonPrefix_1.java
@@ -0,0 +1,26 @@
+class LCP {
+    public String getLongestPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
+
+
+public class LongestCommonPrefix_1{
+    public static void main(String a[]){
+        LCP obj = new LCP();
+
+        String[] strs = {"dog","race","car"};
+        // String[] strs = {"flower","flow","flight"};
+        System.out.println(obj.getLongestPrefix(strs));
+    }
+}
