aa4382dec802a803dd37f5c260b4a43d7b42ae2e
Deep-Saha1925 <dip23447@gmail.com>
2026-05-28 21:51:35 +0530

    Flight class

--- A3/P7/travel/flights/Flight.java
+++ A3/P7/travel/flights/Flight.java
@@ -1,5 +1,19 @@
 package travel.flights;
 
 public class Flight {
-    
+    String flightNumber;
+    String destination;
+    double price;
+
+    public Flight(String flightNumber, String destination, double price) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return "Flight No: " + flightNumber +
+               ", Destination: " + destination +
+               ", Price: " + price;
+    }
 }
