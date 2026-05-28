1184e7c71c447be7596c227a52497d7973dc7d2c
Deep-Saha1925 <dip23447@gmail.com>
2026-05-28 21:55:06 +0530

    Updated bookings

--- A3/P7/travel/bookings/Booking.java
+++ A3/P7/travel/bookings/Booking.java
@@ -1,9 +1,17 @@
-package A3.P7.travel.bookings;
+package travel.bookings;
 
 import travel.flights.Flight;
+import travel.hotels.Hotel;
 
 public class Booking {
-    public void bookFlight(Flight f){
-        System.out.println("Booking flight: " + f.getDetails());
+
+    public void bookFlight(Flight f) {
+        System.out.println("Flight Booked!");
+        System.out.println(f.getDetails());
+    }
+
+    public void bookHotel(Hotel h) {
+        System.out.println("Hotel Booked!");
+        System.out.println(h.getDetails());
     }
 }
