f21be9e1898049e8d2ee76d968b8b1940ab0f812
Deep-Saha1925 <dip23447@gmail.com>
2026-05-28 21:57:43 +0530

    Travel booking main method

--- A3/P7/TravelBooking.java
+++ A3/P7/TravelBooking.java
@@ -1,5 +1,13 @@
-class TravelBooking{
+import travel.flights.Flight;
+import travel.hotels.Hotel;
+
+public class TravelBooking {
     public static void main(String[] args) {
-        
+        Flight flight = new Flight("AI101", "New York", 500.0);
+        Hotel hotel = new Hotel("Grand Hotel", "New York", 200.0);
+
+        Booking booking = new Booking();
+        booking.bookFlight(flight);
+        booking.bookHotel(hotel);
     }
 }
