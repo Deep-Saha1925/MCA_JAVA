d2da0ab50484ef7245d4644eae180d4148fd59c1
Deep-Saha1925 <dip23447@gmail.com>
2026-05-29 21:53:07 +0530

    Main method implementing stock underflow exception

--- A3/P8/EcommerceOrder.java
+++ A3/P8/EcommerceOrder.java
@@ -2,6 +2,12 @@ package A3.P8;
 
 public class EcommerceOrder {
     public static void main(String[] args) {
+        Product p = new Product("Bat", 5);
         
+        try{
+            p.purchase(10);
+        } catch (OutOfStockException e) {
+            System.out.println("Order Failed: " + e.getMessage());
+        }
     }
 }
