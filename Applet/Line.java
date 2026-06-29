5ebedba3112bb7b7c053f9b7d09645e2ba637632
Deep-Saha1925 <dip23447@gmail.com>
2026-06-29 22:29:19 +0530

    Rectangle

--- Applet/Line.java
+++ Applet/Line.java
@@ -5,5 +5,8 @@ import java.awt.Graphics;
 public class Line extends Applet{
     public void paint(Graphics g){
         g.drawLine(20, 20, 200, 20);
+
+        // Rectangle
+        g.drawRect(30, 30, 150, 200);
     }
 }
