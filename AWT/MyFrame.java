db7b53975609f446fc88081388ce24edebb65098
Deep-Saha1925 <dip23447@gmail.com>
2026-06-30 21:24:04 +0530

    Frame using AWT

--- AWT/MyFrame.java
+++ AWT/MyFrame.java
@@ -1,5 +1,10 @@
 import java.awt.*;
 
 public class MyFrame{
-    
+    public static void main(String a[]){
+        Frame f = new Frame("My First Frame");
+
+        f.setSize(400, 300);
+        f.setVisible(true);
+    }
 }
